901402731e18796a6459f21fbcdb14d030a7f576
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2023-08-07 00:31:55 -0700

    Saving data to Real Time Database

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Label, Input, Button } from '@windmill/react-ui';
+import axios from 'axios';
 
 function Home() {
   const [useArconnect, setUseArconnect] = useState(false);
@@ -33,12 +34,43 @@ function Home() {
     setUseArconnect(true);
   };
 
+  const handleSubscribe = () => {
+    // Prepare the data to be sent to the Firebase Realtime Database
+    const data = {
+      walletAddress: useArconnect ? userAddress : userAddress.trim(),
+      email: useArconnect ? email.trim() : email,
+    };
+
+    // Make the POST request to the Firebase Realtime Database
+    axios.post('https://arhubtracker-default-rtdb.firebaseio.com/data.json', data)
+      .then((response) => {
+        alert('Your subscription has been saved successfully! Please check your email (including spam folder)for confirmation.');
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Error saving data to Firebase:', error);
+        // Handle the error here, if required.
+      });
+  };
+
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
         <div className="flex flex-col overflow-y-auto md:flex-row">
           <div className="h-32 md:h-auto md:w-1/2">
             {/* Your image elements here */}
+            <img
+              aria-hidden="true"
+              className="object-cover w-full h-full dark:hidden"
+              src="https://2m2lbxhf7j7z3nwqqgix6ytvw4dmrqnfy6hgcyz4yheazgxoouja.arweave.net/0zSw3OX6f5220IGRf2J1twbIwaXHjmFjPMHIDJrudRI"
+              alt="Office"
+            />
+            <img
+              aria-hidden="true"
+              className="hidden object-cover w-full h-full dark:block"
+              src="https://2m2lbxhf7j7z3nwqqgix6ytvw4dmrqnfy6hgcyz4yheazgxoouja.arweave.net/0zSw3OX6f5220IGRf2J1twbIwaXHjmFjPMHIDJrudRI"
+              alt="Office"
+            />
           </div>
           <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
             <div className="w-full">
@@ -49,9 +81,10 @@ function Home() {
               {useArconnect ? (
                 <>
                   <Label>
-                    <span>Your Wallet Address</span>
-                    <Input className="mt-1" value={userAddress} readOnly />
+                    <span>Your Wallet Address: </span>
+                    <h1 className="mt-1 text-blue-500"> {userAddress} </h1>
                   </Label>
+                  <br></br>
                   <Label>
                     <span>Email</span>
                     <Input className="mt-1" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="sambit@gmail.com" />
@@ -63,6 +96,10 @@ function Home() {
                     <span>Your Wallet Address</span>
                     <Input className="mt-1" value={userAddress} onChange={(e) => setUserAddress(e.target.value)} placeholder="Enter your wallet address manually" />
                   </Label>
+                  <Label>
+                    <span>Email</span>
+                    <Input className="mt-1" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="sambit@gmail.com" />
+                  </Label>
                 </>
               )}
 
@@ -72,11 +109,9 @@ function Home() {
                 </Button>
               )}
 
-              {useArconnect && (
-                <Button tag={Link} to="/login" block className="mt-4">
-                  Subscribe
-                </Button>
-              )}
+              <Button onClick={handleSubscribe} block className="mt-4">
+                Subscribe
+              </Button>
             </div>
           </main>
         </div>
